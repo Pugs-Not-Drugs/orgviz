e643152b3f27492dfa02fbbddb98ece046bd3250
Robert Davies <plexis@outlook.com>
2016-03-19 15:29:33 +0000

    we have the datas

--- modules/crawler.js
+++ modules/crawler.js
@@ -2,21 +2,37 @@ var github = require('octonode');
 
 module.exports = {
   crawl: function () {
-    var client = github.client();
+    var client = github.client('b4d4ee29cbb858da8cb54a3ca80ebb5bc119f2c3');
 
     var ghOrg = client.org('rails');
-    var data = { };
+    var obj = { data: [] };
 
+    function findByLanguage(language, data){
+        for(var langObj in data){
+            if(data[langObj].language === language){
+              return langObj;
+            }
+        }
+        return -1;
+    }
 
     ghOrg.repos(function(err, body){
-      for(var repo in body){
-        var thing = {};
-        thing.language = body[repo].language;
-        thing.count = 1;
-        data.one = thing;
+      for(var repo in body) {
+        var currentLanguage = body[repo].language;
+        var languageIndex = findByLanguage(currentLanguage, obj.data);
+        if( languageIndex !== -1){
+          console.log("are you breaking?");
+          obj.data[languageIndex].count = obj.data[languageIndex].count + 1;
+        } else {
+          console.log("i should be here");
+          var newLanguage = {};
+          newLanguage.language = currentLanguage;
+          newLanguage.count = 1;
+          obj.data.push(newLanguage);
+        }
       }
-      console.log(body);
-      console.log(data);
+      console.log(obj);
+      console.log(err);
     });
 
 
