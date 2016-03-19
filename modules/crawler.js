9fd0d0332a1b4ddd540e20b6d0887993709aeeae
Robert Davies <RobertDavies@Roberts-MacBook-Air.local>
2016-03-19 14:48:30 +0000

    Begun getting data

--- modules/crawler.js
+++ modules/crawler.js
@@ -1,5 +1,25 @@
+var github = require('octonode');
+
 module.exports = {
   crawl: function () {
-    console.log('hi');
+    var client = github.client();
+
+    var ghOrg = client.org('rails');
+    var data = { };
+
+
+    ghOrg.repos(function(err, body){
+      for(var repo in body){
+        var thing = {};
+        thing.language = body[repo].language;
+        thing.count = 1;
+        data.one = thing;
+      }
+      console.log(body);
+      console.log(data);
+    });
+
+
+
   }
 };
